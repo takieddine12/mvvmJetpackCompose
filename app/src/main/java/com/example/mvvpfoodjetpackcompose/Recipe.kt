52d6b4e12f2a68552825474f66493d975d471a81
takieddine12 <algeriahero22@gmail.com>
2020-12-11 13:56:46 +0100

    Mvvm JetPack Compose ..

---
+++ app/src/main/java/com/example/mvvpfoodjetpackcompose/Recipe.kt
@@ -0,0 +1,20 @@
+package com.example.mvvpfoodjetpackcompose
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Recipe (
+    val id : Int? = null ,
+    val title : String? = null,
+    val publisher : String? = null ,
+    val featuredImage : String? = null,
+    val rating : Int? = null ,
+    val sourceUrl : String? = null ,
+    val description : String?  = null ,
+    val cookingInstructions : String? = null ,
+    val ingredients : List<String>? = listOf(),
+    val dateAdded : String? = null ,
+    val dateUpdated : String? = null
+
+) : Parcelable
